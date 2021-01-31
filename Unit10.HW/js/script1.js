e233e379cf3e86e17549e514e8f6f7d0497eb86a
phanduongthuy <thuypd.hubt@gmail.com>
2021-01-31 23:12:12 +0700

    Bai tap buoi 10

---
+++ Unit10.HW/js/script1.js
@@ -0,0 +1,45 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+setTime();
+
+function setTime(){
+  setTimeout(time, 5000)
+}
+
+function time(n) {
+  showSlides(slideIndex += 1)
+  setTime()
+}
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide_img");
+  var li = document.getElementsByClassName("li");
+  if (n > slides.length) {
+    slideIndex = 1
+  }    
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+      
+  for (i = 0; i < li.length; i++) {
+    li[i].className = li[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";  
+  li[slideIndex-1].className += " active";
+
+}
